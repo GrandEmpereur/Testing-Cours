3a6f1d566629ff2c5a2be67972dbd3408626e353
Patrick Bartosik <patrickbartosik@MacBook-Pro-de-Patrick.local>
2024-05-16 14:43:39 +0200

    :green_heart: fix ci

--- src/function.ts
+++ src/function.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Group 1, 3, 5: calldependance function
 export async function calldependance(name: string): Promise<string> {
     console.log(`Calling /action?name=${name}`);
-    const response = await axios.get(`https://testing-cours-118ff93ae024.herokuapp.com//action?name=${name}`);
+    const response = await axios.get(`https://testing-cours-118ff93ae024.herokuapp.com/action?name=${name}`);
     console.log(`Response from /action: ${response.data}`);
     return response.data;
 }
