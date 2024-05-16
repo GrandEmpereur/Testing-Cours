f1747de33c608338fa9372da2e2c21846a9f0280
Patrick Bartosik <patrickbartosik@MacBook-Pro-de-Patrick.local>
2024-05-16 14:45:03 +0200

    :green_heart: fix ci

--- src/test/Testing-e2e.test.ts
+++ src/test/Testing-e2e.test.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 describe('End-to-End Test', () => {
     it('should call /calldependance and return the correct message', async () => {
-        const response = await axios.get('https://testing-cours-118ff93ae024.herokuapp.com/calldependance?name=World');
+        const response = await axios.get('https://testing-cours-118ff93ae024.herokuapp.com/calldependance?name=World').catch((err) => err.response);
         expect(response.data).toBe('Hello, World!');
     });
 });
